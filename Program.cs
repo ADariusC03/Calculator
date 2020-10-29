01bba246a574bf2daff4cc4cdf89d77144e40c53
A'Darius Craig <adariusc@ADariuss-iMac.local>
2020-10-28 22:55:20 -0500

    first set

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Easy made two number and decimal function calculator
+
+            Console.WriteLine("Enter A Number...:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Another Number...:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
